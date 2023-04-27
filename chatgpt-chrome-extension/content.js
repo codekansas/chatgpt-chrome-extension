305994a94044f39f0de5db8163c496f147b2661a
Benjamin Bolte <ben@bolte.cc>
2023-04-27 01:37:54 -0400

    fix

--- chatgpt-chrome-extension/content.js
+++ chatgpt-chrome-extension/content.js
@@ -1,13 +1,21 @@
-setTimeout(() => {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const query = urlParams.get("q");
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const query = urlParams.get("q");
 
-  if (query !== null) {
+if (query !== null) {
+  const interval = setInterval(() => {
     const root = document.querySelectorAll("[data-id=root]");
+    if (root.length === 0) {
+      return;
+    }
+
+    // Clear interval.
+    clearInterval(interval);
+
+    // Set query and click button.
     root[0].innerHTML = query;
     const button = root[0].parentElement.querySelector("button");
     button.removeAttribute("disabled");
     button.click();
-  }
-}, 500);
+  }, 250);
+}
